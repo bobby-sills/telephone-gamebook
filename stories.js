467859e3017a55bbbcf292ecc583da2d2dd2ffd1
Bobby Sills <backsideofdawn@gmail.com>
2025-10-19 11:35:35 +0700

    added fantasy adventure

--- stories.js
+++ stories.js
@@ -1,8 +1,9 @@
 const spaceAdventure = require("./stories/space_adventure.js");
+const fantasyAdventure = require("./stories/fantasy_adventure.js");
 
 const stories = [
   spaceAdventure,
+  fantasyAdventure
 ];
-
 
 module.exports = { stories };
